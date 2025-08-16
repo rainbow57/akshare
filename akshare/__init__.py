eff0a0971d33fdecb3d28b8395d90e0cdefc2f10
叶进 <8058883@qq.com>
2025-08-16 18:54:07 +0800

    feat: :sparkles: 更新gitignore和版本号

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -3140,7 +3140,7 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.17.31 fix: fix index_neei_cx interface
 """
 
-__version__ = "1.17.31"
+__version__ = "1.17.31.1"
 __author__ = "AKFamily"
 
 import sys
